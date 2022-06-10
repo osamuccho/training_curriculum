c0f6dd6da78c1e97e5d204f0a5fdf7926c98e6b8
osamuccho <basket.o.y@icloud.com>
2022-06-10 12:32:44 +0900

    issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,6 +3,7 @@ class CalendarsController < ApplicationController
   # １週間のカレンダーと予定が表示されるページ
   def index
     getWeek
+    # binding.pry
     @plan = Plan.new
   end
 
@@ -15,7 +16,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
@@ -36,6 +37,7 @@ class CalendarsController < ApplicationController
       end
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
+      binding.pry
     end
 
   end
